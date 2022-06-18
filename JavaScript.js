67bd1a006a674e2599cfca1e7affdeaeec50c101
P-Matejovic <107559716+P-Matejovic@users.noreply.github.com>
2022-06-18 10:59:02 +0200

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,56 @@
 console.log("JavaScript is running !");
 
 let bookCount = 10;
-for (let i = 0; i < bookCount; i++) {
+/*for (let i = 0; i < bookCount; i++) {
+    console.log(i);
+
+}*/
+
+/*for (let i = 0; i < bookCount+11; i++) {
+    console.log(i);
     
-}
+}*/
+
+/* Excercise1*/
+/*
+let printNumbersTill = (j) => {
+    for (let i = 0; i < j+1; i++) {
+        console.log(i);
+}};
+//printNumbersTill(15);
+printNumbersTill(120);
+
+*/
+//Excercise 2
+/*
+function func1 (YourName) {
+    console.log("Good morning " + YourName + "!")
+};
+func1("Pavel");
+*/
+
+//Excercise 1 for loops
+/*
+let arry = [0, 3, 6, 7, 9];
+function printValues (array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);}
+};
+
+printValues(arry);
+*/
+/*
+let myNumbers = [10, 20, 30, 50, 12];
+
+myNumbers.forEach((number)=> {
+console.log(number)
+});
+*/
+
+//excercise Foreach
+
+let arry = [0, 3, 6, 7, 9];
+
+arry.forEach((number)=> {
+    console.log(number);
+});
